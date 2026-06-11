a77fe04183e66da16587db05013047af90502a82
krystoham <krystof.ham@volny.cz>
2026-06-11 20:44:38 +0200

    recursive approach, i dont mind

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int mode = 3; 
+#define MAX_WORD_LEN 50
+#define ALL_KEYS_COUNT 10
+#define ALL_TOKENS_COUNT 10
+
+int mode = 3;
 
 typedef struct {
-    char value[50];
+    char value[MAX_WORD_LEN];
     int weight;
 } WeightedWord;
 
@@ -17,26 +22,15 @@ typedef struct {
     WeightedWord american_spell;
     WeightedWord european_spell;
     WeightedWord short_spell;
-    char key_id[50];
-    char key_id2[50];
+    char key_id[MAX_WORD_LEN];
+    char key_id2[MAX_WORD_LEN];
 } human;
 
-typedef struct {
-    WeightedWord one;
-    WeightedWord two;
-    WeightedWord three;
-    WeightedWord four;
-    WeightedWord five;
-    WeightedWord six;
-    WeightedWord seven;
-    WeightedWord eight;
-    WeightedWord none;
-    WeightedWord nine;
-} keys;
+long long total_words = 0;
+long long total_bytes = 0;
 
-human scanfortokens () {
+human scanfortokens() {
     human target;
-
     target.name.weight = 1;
     target.surname.weight = 1;
     target.pet.weight = 3;
@@ -46,76 +40,30 @@ human scanfortokens () {
     target.european_spell.weight = 1;
     target.short_spell.weight = 2;
 
-    printf("Enter name: \n");
-    scanf("%49s", target.name.value);
+    printf("Enter name: \n"); scanf("%49s", target.name.value);
+    printf("Enter surname: \n"); scanf("%49s", target.surname.value);
+    printf("Enter pet: \n"); scanf("%49s", target.pet.value);
+    printf("Enter team: \n"); scanf("%49s", target.team.value);
+    printf("Enter car: \n"); scanf("%49s", target.car.value);
+    printf("Enter american spell: \n"); scanf("%49s", target.american_spell.value);
+    printf("Enter european spell: \n"); scanf("%49s", target.european_spell.value);
+    printf("Enter year only: \n"); scanf("%49s", target.short_spell.value);
+    printf("Enter first key identifier: \n"); scanf("%49s", target.key_id);
+    printf("Enter second key identifier: \n"); scanf("%49s", target.key_id2);
 
-    printf("Enter surname: \n");
-    scanf("%49s", target.surname.value);
-
-    printf("Enter pet: \n");
-    scanf("%49s", target.pet.value);
-
-    printf("Enter team: \n");
-    scanf("%49s", target.team.value);
-
-    printf("Enter car: \n");
-    scanf("%49s", target.car.value);
-
-    printf("Enter american spell: \n");
-    scanf("%49s", target.american_spell.value);
-
-    printf("Enter european spell: \n");
-    scanf("%49s", target.european_spell.value);
-
-    printf("Enter year only: \n");
-    scanf("%49s", target.short_spell.value);
-
-    printf("Enter first key identifier: \n");
-    scanf("%49s", target.key_id);
-
-    printf("Enter second key identifier: \n");
-    scanf("%49s", target.key_id2);
-    
     return target;
 }
 
-keys scanforkeys() {
-    keys my_keys;
-
-    strcpy(my_keys.one.value, "123");
-    my_keys.one.weight = 1;
-
-    strcpy(my_keys.two.value, "12345");
-    my_keys.two.weight = 2;
-
-    strcpy(my_keys.three.value, "123456789");
-    my_keys.three.weight = 2;
-
-    strcpy(my_keys.four.value, "!");
-    my_keys.four.weight = 2;
-
-    strcpy(my_keys.five.value, "*");
-    my_keys.five.weight = 2;
-
-    strcpy(my_keys.six.value, ".");
-    my_keys.six.weight = 1;
-
-    strcpy(my_keys.seven.value, ":");
-    my_keys.seven.weight = 3;
-
-    strcpy(my_keys.eight.value, "?");
-    my_keys.eight.weight = 3;
-
-    strcpy(my_keys.none.value, "");
-    my_keys.none.weight = 1;
-
-    strcpy(my_keys.nine.value, "321");
-    my_keys.nine.weight = 2;
-
-    return my_keys;
+void init_keys(WeightedWord all_keys[ALL_KEYS_COUNT]) {
+    char *values[] = {"123", "12345", "123456789", "!", "*", ".", ":", "?", "", "321"};
+    int weights[] = {1, 2, 2, 2, 2, 1, 3, 3, 1, 2};
+    for (int i = 0; i < ALL_KEYS_COUNT; i++) {
+        strcpy(all_keys[i].value, values[i]);
+        all_keys[i].weight = weights[i];
+    }
 }
 
-void ConvertStructHumanToField(human target, WeightedWord all_words[10]) {
+void convert_human_to_array(human target, WeightedWord all_words[ALL_TOKENS_COUNT]) {
     all_words[0] = target.name;
     all_words[1] = target.surname;
     all_words[2] = target.pet;
@@ -124,44 +72,96 @@ void ConvertStructHumanToField(human target, WeightedWord all_words[10]) {
     all_words[5] = target.american_spell;
     all_words[6] = target.european_spell;
     all_words[7] = target.short_spell;
-
-    strcpy(all_words[8].value, target.key_id);
-    all_words[8].weight = 1;
-
-    strcpy(all_words[9].value, target.key_id2);
-    all_words[9].weight = 1;
+    strcpy(all_words[8].value, target.key_id);   all_words[8].weight = 1;
+    strcpy(all_words[9].value, target.key_id2);  all_words[9].weight = 1;
 }
 
-void OneWordPrint(WeightedWord all_keys[10], WeightedWord all_words[10]) {
-    int value;
-    long long total_words = 0;
-    long long total_bytes = 0;
+void uppercase_word(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        if (src[i] >= 'a' && src[i] <= 'z') {
+            dest[i] = src[i] - 32;
+        } else {
+            dest[i] = src[i];
+        }
+        i++;
+    }
+    dest[i] = '\0';
+}
 
-    for (int a = 0; a < 10; a++) {
-        for (int b = 0; b < 10; b++) {
-            for (int c = 0; c < 10; c++) {
-                value = all_keys[a].weight + (all_words[b].weight * 2) + all_keys[c].weight;
-                
-                int max_allowed_weight = 10;
-                if (mode == 2) max_allowed_weight = 15;
-                if (mode == 3) max_allowed_weight = 105;
+void capitalize_word(char *dest, const char *src) {
+    strcpy(dest, src);
+    if (dest[0] >= 'a' && dest[0] <= 'z') {
+        dest[0] -= 32;
+    }
+}
 
-                if (value <= max_allowed_weight) {
-                    char key[200] = "";
-                    strcat(key, all_keys[a].value);
-                    strcat(key, all_words[b].value);
-                    strcat(key, all_keys[c].value);
-                    
-                    // printf("%s\n", key);
-                    
-                    total_words++;
-                    total_bytes += strlen(key) + 1;
-                }
+void print_and_count(const char *password) {
+    printf("%s\n", password);
+    total_words++;
+    total_bytes += strlen(password) + 1; // +1 za znak \n
+}
+
+
+void generate_rec(int current_depth, int target_depth, int current_weight, int max_weight, const char *prefix, WeightedWord all_keys[ALL_KEYS_COUNT], WeightedWord all_words[ALL_TOKENS_COUNT]) {
+    
+    for (int k = 0; k < ALL_KEYS_COUNT; k++) {
+        int weight_with_key = current_weight + all_keys[k].weight;
+        
+        char temp_buffer[1024];
+        snprintf(temp_buffer, sizeof(temp_buffer), "%s%s", prefix, all_keys[k].value);
+        
+        if (current_depth == target_depth) {
+            if (weight_with_key <= max_weight) {
+                print_and_count(temp_buffer);
             }
+            continue; 
+        }
+        
+        for (int w = 0; w < ALL_TOKENS_COUNT; w++) {
+            int next_weight = weight_with_key + (all_words[w].weight * 2);
+            
+            if (next_weight > max_weight) continue;
+            
+            char word_lower[MAX_WORD_LEN];
+            char word_cap[MAX_WORD_LEN];
+            char word_upper[MAX_WORD_LEN];
+            
+            strcpy(word_lower, all_words[w].value);
+            capitalize_word(word_cap, all_words[w].value);
+            uppercase_word(word_upper, all_words[w].value);
+            
+            char next_prefix[1024];
+            
+            snprintf(next_prefix, sizeof(next_prefix), "%s%s", temp_buffer, word_lower);
+            generate_rec(current_depth + 1, target_depth, next_weight, max_weight, next_prefix, all_keys, all_words);
+            
+            snprintf(next_prefix, sizeof(next_prefix), "%s%s", temp_buffer, word_cap);
+            generate_rec(current_depth + 1, target_depth, next_weight, max_weight, next_prefix, all_keys, all_words);
+            
+            snprintf(next_prefix, sizeof(next_prefix), "%s%s", temp_buffer, word_upper);
+            generate_rec(current_depth + 1, target_depth, next_weight, max_weight, next_prefix, all_keys, all_words);
         }
     }
+}
 
-    printf("\n--- STATS ---\n");
+void run_password_generator(int word_count, WeightedWord all_keys[ALL_KEYS_COUNT], WeightedWord all_words[ALL_TOKENS_COUNT]) {
+    int max_allowed_weight = 100; // Výchozí pro 3 slova, mód 3
+    
+    if (word_count == 1) {
+        max_allowed_weight = (mode == 2) ? 15 : ((mode == 3) ? 105 : 10);
+    } else if (word_count == 2) {
+        max_allowed_weight = (mode == 2) ? 20 : ((mode == 3) ? 105 : 18);
+    } else if (word_count == 3) {
+        max_allowed_weight = (mode == 2) ? 25 : ((mode == 3) ? 100 : 20);
+    }
+    
+    total_words = 0;
+    total_bytes = 0;
+    
+    generate_rec(0, word_count, 0, max_allowed_weight, "", all_keys, all_words);
+    
+    printf("\n--- STATS (%d Word%s) ---\n", word_count, word_count > 1 ? "s" : "");
     printf("Total passwords: %lld\n", total_words);
     printf("Total size: %lld Bytes\n", total_bytes);
     printf("Total size: %lld bits\n", total_bytes * 8);
@@ -170,145 +170,20 @@ void OneWordPrint(WeightedWord all_keys[10], WeightedWord all_words[10]) {
         printf("Avg size per word: %.2f Bytes (%.2f bits)\n", avg_bytes, avg_bytes * 8);
     }
 }
-
-
-void TwoWordPrint(WeightedWord all_keys[10], WeightedWord all_words[10]) {
-    int value;
-    long long total_words = 0;
-    long long total_bytes = 0;
-
-    for (int a = 0; a < 10; a++) {
-        for (int b = 0; b < 10; b++) {
-            for (int c = 0; c < 10; c++) {
-                for (int d = 0; d < 10; d++) {
-                    for (int e = 0; e < 10; e++) {
-                        value = all_keys[a].weight + (all_words[b].weight * 2) + all_keys[c].weight + (all_words[d].weight * 2) + all_keys[e].weight;
-                        
-                        int max_allowed_weight = 18;
-                        if (mode == 2) max_allowed_weight = 20;
-                        if (mode == 3) max_allowed_weight = 105;
-
-                        if (value <= max_allowed_weight) {
-                            char key[200] = "";
-                            strcat(key, all_keys[a].value);
-                            strcat(key, all_words[b].value);
-                            strcat(key, all_keys[c].value);
-                            strcat(key, all_words[d].value);
-                            strcat(key, all_keys[e].value);
-                            // printf("%s\n", key);
-                            
-                            total_words++;
-                            total_bytes += strlen(key) + 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    printf("\n--- STATS ---\n");
-    printf("Total passwords: %lld\n", total_words);
-    printf("Total size: %lld Bytes\n", total_bytes);
-    printf("Total size: %lld bits\n", total_bytes * 8);
-    if (total_words > 0) {
-        double avg_bytes = (double)total_bytes / total_words;
-        printf("Avg size per word: %.2f Bytes (%.2f bits)\n", avg_bytes, avg_bytes * 8);
-    }
-}
-
-void ThreeWordPrint(WeightedWord all_keys[10], WeightedWord all_words[10]) {
-    int value;
-    long long total_words = 0;
-    long long total_bytes = 0;
-
-    for (int a = 0; a < 10; a++) {
-        for (int b = 0; b < 10; b++) {
-            for (int c = 0; c < 10; c++) {
-                for (int d = 0; d < 10; d++) {
-                    for (int e = 0; e < 10; e++) {
-                        for (int f = 0; f < 10; f++) {
-                            for (int g = 0; g < 10; g++) {
-                                value = all_keys[a].weight + (all_words[b].weight * 2) + all_keys[c].weight + (all_words[d].weight * 2) + all_keys[e].weight + (all_words[f].weight * 2) + all_keys[g].weight;
-                                char word1[50], word2[50], word3[50];
-                                strcpy(word1, all_words[b].value);
-                                strcpy(word2, all_words[d].value);
-                                strcpy(word3, all_words[f].value);
-
-                                char key1[200] = "";
-                                sprintf(key1, "%s%s%s%s%s%s%s", all_keys[a].value, word1, all_keys[c].value, word2, all_keys[e].value, word3, all_keys[g].value);
-
-                                if (word1[0] >= 'a' && word1[0] <= 'z') word1[0] -= 32; 
-                                if (word2[0] >= 'a' && word2[0] <= 'z') word2[0] -= 32;
-                                if (word3[0] >= 'a' && word3[0] <= 'z') word3[0] -= 32;
-
-                                char key2[200] = "";
-                                sprintf(key2, "%s%s%s%s%s%s%s", all_keys[a].value, word1, all_keys[c].value, word2, all_keys[e].value, word3, all_keys[g].value);
-
-                                for (int i = 0; word1[i] != '\0'; i++) {
-                                    if (word1[i] >= 'a' && word1[i] <= 'z') word1[i] -= 32;
-                                }
-                                for (int i = 0; word2[i] != '\0'; i++) {
-                                    if (word2[i] >= 'a' && word2[i] <= 'z') word2[i] -= 32;
-                                }
-                                for (int i = 0; word3[i] != '\0'; i++) {
-                                    if (word3[i] >= 'a' && word3[i] <= 'z') word3[i] -= 32;
-                                }
-
-                                char key3[200] = "";
-                                sprintf(key3, "%s%s%s%s%s%s%s", all_keys[a].value, word1, all_keys[c].value, word2, all_keys[e].value, word3, all_keys[g].value);
-
-                                int max_allowed_weight = 20;
-                                if (mode == 2) max_allowed_weight = 25;
-                                if (mode == 3) max_allowed_weight = 100;
-
-                                if (value <= max_allowed_weight) {
-                                    printf("%s \n %s \n %s", key1, key2, key3);
-                                    // printf("%s\n", key3);
-                                    
-                                    total_words += 3;
-                                    total_bytes += (strlen(key1) + 1) + (strlen(key2) + 1) + (strlen(key3) + 1);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    printf("\n--- STATS ---\n");
-    printf("Total passwords: %lld\n", total_words);
-    printf("Total size: %lld Bytes\n", total_bytes);
-    printf("Total size: %lld bits\n", total_bytes * 8);
-    if (total_words > 0) {
-        double avg_bytes = (double)total_bytes / total_words;
-        printf("Avg size per word: %.2f Bytes (%.2f bits)\n", avg_bytes, avg_bytes * 8);
-    }
-}
-
-
 
 int main() {
-    printf("ZADAVEJ JENOM MALE PISMENA\n");
+    printf("Enter lower case only\n");
     human target = scanfortokens();
-    keys primary_keys = scanforkeys();
     
-    WeightedWord all_keys[10];
-    all_keys[0] = primary_keys.one;
-    all_keys[1] = primary_keys.two;
-    all_keys[2] = primary_keys.three;
-    all_keys[3] = primary_keys.four;
-    all_keys[4] = primary_keys.five;
-    all_keys[5] = primary_keys.six;
-    all_keys[6] = primary_keys.seven;
-    all_keys[7] = primary_keys.eight;
-    all_keys[8] = primary_keys.none;
-    all_keys[9] = primary_keys.nine;
-
-    WeightedWord all_tokens[10];
-    ConvertStructHumanToField(target, all_tokens);
-
-    OneWordPrint(all_keys, all_tokens);
-    TwoWordPrint(all_keys, all_tokens);
+    WeightedWord all_keys[ALL_KEYS_COUNT];
+    init_keys(all_keys);
+    
+    WeightedWord all_tokens[ALL_TOKENS_COUNT];
+    convert_human_to_array(target, all_tokens);
+    
+    run_password_generator(1, all_keys, all_tokens);
+    run_password_generator(2, all_keys, all_tokens);
+    run_password_generator(3, all_keys, all_tokens);
+    
     return 0;
 }
